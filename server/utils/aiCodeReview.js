51818aafa74d71c904170ee5d1bc7f0413bb2584
ARRA SRIHARSHA <harsha@Harshas-MacBook-Pro.local>
2025-06-03 18:14:13 +0530

    Modified prompt

--- server/utils/aiCodeReview.js
+++ server/utils/aiCodeReview.js
@@ -9,7 +9,7 @@ export const aiCodeReview = async (code) => {
     const response = await genAI.models.generateContent({
         model: "gemini-2.0-flash",
         contents: `
-        review the code
+        review the code.Use clean text.Do not add *'s and make it look messy.Add new lines wherever necessary
          ${code}
         `,
       });
