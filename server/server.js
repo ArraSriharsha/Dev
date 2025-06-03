9ea53c8f7ab7e26be42a36c0a2f1c7e555e6fd90
ARRA SRIHARSHA <harsha@Harshas-MacBook-Pro.local>
2025-06-03 23:26:55 +0530

    Two urls

--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const allowedOrigins = process.env.CLIENT_URL.split(',');
 // CORS configuration
 app.use(cors({
-    origin: process.env.CLIENT_URL ,
+    origin: allowedOrigins ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
